788a8b7bbfee628d89944caf81b5432953942e0d
hyperkarma <randy_pan@yahoo.com>
2020-08-30 08:10:27 +0100

    Updates

---
+++ precache-manifest.3cc28d5980d2ff70d81e38e1ecfb2d64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fa9adc9bf30959db9c02f6cd9b3fa3",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "df90a1710e37d675f9d1",
+    "url": "/react-employee-directory/static/js/2.f7b3c719.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/react-employee-directory/static/js/2.f7b3c719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bd949a861f12ac84a3",
+    "url": "/react-employee-directory/static/js/main.50dc097d.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  }
+]);
