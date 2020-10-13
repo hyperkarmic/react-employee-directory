9f85560da37922c794e405b9f8460676cf502159
hyperkarma <randy_pan@yahoo.com>
2020-10-13 15:48:40 +0100

    text removed

--- src/components/Jumbo/index.js
+++ src/components/Jumbo/index.js
@@ -4,10 +4,7 @@ const Jumbo = () => {
   return (
     <div class="jumbotron text-center">
       <h1 class="display-4">Employee Directory</h1>
-      <p class="lead">
-        Click on carets to filter by heading or use the search box to narrow
-        your results
-      </p>
+      
       <hr class="my-4" />
     </div>
   );
